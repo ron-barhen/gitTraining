47fddf1c42928b0b69fbf85e4f7a4545cc906935
ron-barhen <barhen.ron@gmail.com>
2022-09-03 12:48:01 +0200

    Fourth and Fifth line

--- test1.c
+++ test1.c
@@ -5,5 +5,7 @@ int main()  {
     printf("First line of code in test1.c\n");
     printf("Second line of test1.c\n");
     printf("Third line of test1.c\n");
+    printf("Fourth line of test1.c\n");
+    printf("Fifth line of test1.c\n");
     return 0;
 }
