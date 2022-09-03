14a7113aa1f54db0c90d2bc86e3796c91addd6dc
ron-barhen <barhen.ron@gmail.com>
2022-09-03 13:06:21 +0200

    Last line  changed

--- test1.c
+++ test1.c
@@ -5,6 +5,6 @@ int main()  {
     printf("First line of code in test1.c\n");
     printf("Second line of test1.c\n");
     printf("Third line of test1.c\n");
-    printf("Fourth line of test1.c\n");
+    printf("Fourth line1 of test1.c\n");
     return 0;
 }
